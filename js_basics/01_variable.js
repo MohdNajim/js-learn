c4a5bb875e03b20f51ac922b8913e846922f98ac
MOHD NAJIM <115221985+MohdNajim@users.noreply.github.com>
2024-10-16 17:16:47 +0000

    confing done

---
+++ js_basics/01_variable.js
@@ -0,0 +1,9 @@
+const accountId= 128448;
+let accountName= "NajimRajput";
+let accountEmail="hdf@gmail.com";
+let accountAddress= "Kolkata";
+var accountholderAge=25;
+accountAddress ="mumbai";
+var accountholderAge=30;
+
+console.table([accountName,accountId,accountEmail,accountAddress,accountholderAge]);
