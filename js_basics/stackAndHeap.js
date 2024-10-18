20128e6bad996c04b6b23a1fe9b653e50d180b9b
MOHD NAJIM <115221985+MohdNajim@users.noreply.github.com>
2024-10-18 18:13:02 +0000

    config done in stackandheap

---
+++ js_basics/stackAndHeap.js
@@ -0,0 +1,22 @@
+let isNumber = 232;  
+//changeNumber=isNumber;  premitive datatype m copy milti h 
+//changeNumber=432;
+//console.log(isNumber);
+//console.log(changeNumber);
+
+//object(Non premetive data types)   Heap Memory m referance milta h 
+
+let userOne ={
+    userEmail:"Najim@google.com",
+    userUpi:"98837373@ybl"
+}
+
+let userTwo=userOne;
+   userTwo.userEmail="hdfc@gmail.com"
+
+console.log(userOne.userEmail);
+console.log(userTwo.userEmail);
+
+
+
+
